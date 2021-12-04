c27f33be9e6b85aa3f7aa585977c5b4fc098f363
Ben Tracy <benatracy@gmail.com>
2021-12-04 21:02:47 +0000

    Actually finished day 1

--- aoc/src/bin/day1/main.rs
+++ aoc/src/bin/day1/main.rs
@@ -1,4 +1,3 @@
-use std::env;
 use std::fs;
 use std::io::BufRead;
 use std::io::BufReader;
@@ -10,9 +9,14 @@ fn main() {
         .lines()
         .map(|l| l.expect("Parse failure"))
         .collect();
-
+    let values: Vec<i32> = lines
+        .iter()
+        .map(|str| str.trim().parse::<i32>().unwrap())
+        .collect();
     let mut increases: i32 = 0;
     let mut prev = i32::MAX;
+
+    // Part 1
     for line in lines {
         match line.trim().parse::<i32>() {
             Ok(value) => {
@@ -26,4 +30,18 @@ fn main() {
     }
 
     println!("Number of increases: {:?}", increases);
+
+    // Part 2
+    let window_size = 3;
+    let mut prev_sum: i32 = i32::MAX;
+    let mut current_sum: i32;
+    increases = 0;
+    for i in 0..(values.len() - window_size + 1) {
+        current_sum = values[i..(i + window_size)].iter().sum();
+        if current_sum > prev_sum {
+            increases += 1;
+        }
+        prev_sum = current_sum;
+    }
+    println!("Number of increases (window version): {:?}", increases);
 }
